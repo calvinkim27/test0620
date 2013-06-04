9b5976cb243ea313e61e02113e92bb2edd3853a1
Eunchong Yu <kroisse@gmail.com>
2013-06-04 17:08:44 +0900

    Add frontend bases

---
+++ midauth/web/static/js/main.js
@@ -0,0 +1,7 @@
+(function ($, undefined) {
+    "use strict";
+
+    $(document).ready(function () {
+        $('.navbar a[data-toggle=tooltip]').tooltip({placement: 'bottom', container: 'body'});
+    });
+})(jQuery);
